e208601120eee08ef5fe2dc14762ee33d0c79b6f
TheOnlyCreator <wassil.janssen@gmail.com>
2015-10-18 01:34:49 +0300

    That is some code!

--- Objects/Button.lua
+++ Objects/Button.lua
@@ -7,6 +7,7 @@
 
 function Button(x,y,width,height,xText,yText,text,textColor,backgroundColor,textColorOnPress,backgroundColorOnPress,onRightClick,onLeftClick)
 	--Private--
+	local events = {}
 	local x = x or 1
 	local y = y or 1
 	local width = width or 11
@@ -20,6 +21,8 @@ function Button(x,y,width,height,xText,yText,text,textColor,backgroundColor,text
 	local backgroundColorOnPress = backgroundColorOnPress or colors.black
 	local onRightClick = onRightClick or function() end
 	local onLeftClick = onLeftClick or function() end
+	setfenv(onRightClick,getfenv())
+	setfenv(onLeftClick,getfenv())
 	--Public--
 	local self = {}
 	function self.draw(isPressed, xOffset, yOffset)
@@ -75,6 +78,36 @@ function Button(x,y,width,height,xText,yText,text,textColor,backgroundColor,text
 		backgroundColorOnPress = targs.backgroundColorOnPress or backgroundColorOnPress
 		onRightClick = targs.onRightClick and (type(targs.onRightClick) == "string" and (function() k,func = loadstring(targs.onRightClick) if not k then return function()end else setfenv(func,_G) end return func end)() or targs.onRightClick) or onRightClick
 		onLeftClick = targs.onLeftClick and (type(targs.onLeftClick) == "string" and (function() k,func = loadstring(targs.onLeftClick) if not k then return function()end else setfenv(func,_G) end return func end)() or targs.onLeftClick) or onLeftClick
+		setfenv(onRightClick,getfenv())
+		setfenv(onLeftClick,getfenv())
+	end
+	function self.addEvent(event,func)
+		if events[event] then
+			events[event][#events[event]+1] = func
+		else
+			events[event][1] = func
+		end
+	end
+	function self.run(stop,xOffset,yOffset)
+		local finalX = xOffset and (xOffset - 1 + x) or x
+		local finalY = yOffset and (yOffset - 1 + y) or y
+		while true do
+			e = {os.pullEvent()}
+			if events[e] then
+				for i,v in pairs(events[e]) do
+					v(unpack(e))
+				end
+			end
+			if e[1] == "mouse_click" then
+				if finalX <= e[3] and e[3] <= finalX+width-1 and finalY <= e[4] and e[4] <= finalY+height-1 then
+					if e[2] == 1 then
+						onLeftClick()
+					elseif e[2] == 2 then
+						onRightClick()
+					end
+				end
+			end
+		end
 	end
 	return self
 end
--- Test.lua
+++ Test.lua
@@ -1,5 +1,18 @@
 os.loadAPI("Pixel/Pixel")
-button = Pixel.Button()
+button = Pixel.Button(3,4)
+input = {
+	x = 3,
+	y = 4,
+	width = 4,
+	height = 3,
+	xText = 2,
+	yText = 2,
+	text = "OK",
+	textColor = colors.orange,
+	backgroundColor = colors.black,
+	textColorOnPress = colors.white,
+	backgroundColorOnPress = colors.cyan,
+	onRightClick = function() term.setCursorPos(20,13) print("wow") end,
+}
+button.set(input)
 button.draw()
-print()
-print(textutils.serialize(button.get(true)))
